d3687f1a565905323dc49b17c8590c32da409e58
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-05-27 23:05:19 +0530

    Add files via upload

---
+++ Contest-295.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    internal class Contest_295
+    {
+        //Contests URL: https://atcoder.jp/contests/abc295/tasks
+
+        #region A - Probably English
+        public static void Probably_English()
+        {
+            int Length = int.Parse(Console.ReadLine());
+            string[] Input = Console.ReadLine().Split();
+            int count = 0;
+            foreach (string word in Input)
+            {
+                if (word.Equals("and") || word.Equals("that") || word.Equals("not") || word.Equals("the") || word.Equals("you"))
+                {
+                    count++;
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+        #endregion
+
+        #region C - Socks
+        public static void Socks()
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] StrArray = Console.ReadLine().Split(" ");
+            Array.Sort(StrArray);
+            int Ans = 0;
+            int i = 0;
+            while (i + 1 < N)
+            {
+                if (StrArray[i] == StrArray[i + 1])
+                {
+                    Ans++;
+                    i += 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(Ans);
+        }
+        #endregion
+
+        #region 
+        public static string Problem4()
+        {
+            int a = int.Parse(Console.ReadLine());
+            string Result = string.Empty;
+            string[] input = Console.ReadLine().Split("|");
+            if (input[1].Contains("*"))
+            {
+                Result = "In";
+            }
+            else
+            {
+                Result = "Out";
+            }
+            return Result;
+        }
+        #endregion
+
+    }
+}
---
+++ Contest-303.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    internal class Contest_303
+    {
+        //https://atcoder.jp/contests/abc303/tasks
+        #region Similar_String
+        public static void Similar_String(int value)
+        {
+            string S = Console.ReadLine();
+            string T = Console.ReadLine();
+            string Result = "Yes";
+            int count = 0;
+            for (int i = 0; i < value; i++)
+            {
+                if ((S[i].Equals(T[i]))) { }
+                else if ((S[i].Equals('0') && T[i].Equals('o')) || (S[i].Equals('o') && T[i].Equals('0'))) { }
+                else if ((S[i].Equals('1') && T[i].Equals('l')) || (S[i].Equals('l') && T[i].Equals('1'))) { }
+                else
+                {
+                    Result = "No";
+                    break;
+                }
+            }
+            Console.WriteLine(Result);
+        }
+        #endregion
+
+        #region Discord
+        public static void Discord()
+        {
+            int[] inputs = Console.ReadLine().Trim().Split(' ').Select(x => int.Parse(x)).ToArray();
+            int amount = inputs[0];
+            int times = inputs[1];
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < times; i++)
+            {
+                int[] lines = Console.ReadLine().Trim().Split(' ').Select(y => int.Parse(y)).ToArray();
+                for (int j = 0; j < amount - 1; j++)
+                {
+                    set.Add(lines[j].ToString("D2") + lines[j + 1].ToString("D2"));
+                    set.Add(lines[j + 1].ToString("D2") + lines[j].ToString("D2"));
+                }
+            }
+
+            Console.WriteLine((amount * (amount - 1) / 2) - (set.Count / 2));
+
+            //return 0;
+        }
+        #endregion
+
+        #region Dash
+        public static void Dash()
+        {
+            string[] NMHK = Console.ReadLine().Split(" ");
+            int n = int.Parse(NMHK[0]);
+            int m = int.Parse(NMHK[1]);
+            int h = int.Parse(NMHK[2]);
+            int k = int.Parse(NMHK[3]);
+
+            char[] s = Console.ReadLine().ToCharArray();
+
+            HashSet<string> item = new HashSet<string>();
+            for (int i = 0; i < m; i++)
+            {
+                string xy = Console.ReadLine();
+                item.Add(xy);
+            }
+
+            int x = 0;
+            int y = 0;
+            int power = h;
+            string answer = "Yes";
+            for (int i = 0; i < n; i++)
+            {
+                power--;
+
+                if (power < 0)
+                {
+                    answer = "No";
+                    break;
+                }
+
+                if (s[i] == 'R')
+                    x++;
+                else if (s[i] == 'L')
+                    x--;
+                else if (s[i] == 'U')
+                    y++;
+                else if (s[i] == 'D')
+                    y--;
+
+                if (power < k)
+                {
+                    string key = x.ToString() + " " + y.ToString();
+
+                    if (item.Contains(key))
+                    {
+                        power = k;
+                        item.Remove(key);
+                    }
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+        #endregion
+
+    }
+}
