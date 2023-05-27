4d16a00ae36957fae8ef6b04a7453769764db22a
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-05-27 23:04:10 +0530

    Add files via upload

---
+++ Series2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    internal class Series2
+    {
+        public static void CapsLock(string Word)
+        {
+            Word = Word.ToLower();
+            char[] ch = Word.ToCharArray();
+            for(int i=0; i<ch.Length;i++)
+            {
+                if (Word[0]>=97 && Word[0]<=122)
+                {
+                    ch[0] = (char)(ch[0] - 32);
+                }
+                Console.Write(ch[i]);
+            }
+        }
+    }
+}
